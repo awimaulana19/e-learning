c5a15ab911347bf9b5d5f9bd7b58301b5ae392ee
awimaulana19 <awimaulana19@gmail.com>
2023-07-26 23:35:22 +0800

    Mongo DB Cloud

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/e-learning');
+mongoose.connect('mongodb+srv://awimaulana19:Awimaulana123@cluster0.lnqkqdg.mongodb.net/e-learning?retryWrites=true&w=majority');
