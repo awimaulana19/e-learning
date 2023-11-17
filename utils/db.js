dd684cf9f8497e8a649ac31fa4262af636897785
awimaulana19 <awimaulana19@gmail.com>
2023-11-17 13:42:03 +0800

    trigger

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const db = process.env.DB_CONNECTION_STRING;
 // const db = 'mongodb://127.0.0.1:27017/e-learning';
+const db = process.env.DB_CONNECTION_STRING;
+
 mongoose.connect(db);
 
 module.exports = { db };
